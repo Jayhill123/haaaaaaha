cab7d760d75332f933dd3446e0854df03aa2af42
Jayhill123 <55261864+Jayhill123@users.noreply.github.com>
2019-12-05 18:25:09 -0600

    need more

---
+++ doc/haaaha unity project/Assets/scripts/loadscreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadscreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+               
+           public void LoadScene()
+            {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("SampleScene");
+            }
+ }
